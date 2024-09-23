016a7fe1d9127309c0a8b4cfedf3233f5ccd99ec
muhammadbasitobaid <muhammadbasitobaid@gmail.com>
2024-09-23 05:24:12 +0500

    started layout development

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
-import EmailIcon from "src/components/shared/icons/EmailIcon";
-import GithubIcon from "src/components/shared/icons/GithubIcon";
-import LinkedInIcon from "src/components/shared/icons/LinkedInIcon";
-import TwitterIcon from "src/components/shared/icons/TwitterIcon";
-import UpworkIcon from "src/components/shared/icons/UpworkIcon";
-import BagIcon from "src/components/shared/icons/BagIcon";
-import FolderIcon from "src/components/shared/icons/FolderIcon";
-import HomeIcon from "src/components/shared/icons/HomeIcon";
-import ThoughtsIcon from "src/components/shared/icons/ThoughtsIcon";
-import ToolIcon from "src/components/shared/icons/ToolIcon";
 import "src/index.css";
+import Container from "src/components/shared/Container";
 
 const App = () => {
   return (
-    <div className="flex space-x-4 bg-black">
-      <BagIcon />
-      <FolderIcon />
-      <HomeIcon />
-      <ThoughtsIcon />
-      <TwitterIcon />
-      <EmailIcon />
-      <GithubIcon />
-      <LinkedInIcon />
-      <UpworkIcon />
-      <ToolIcon />
+    <div className="bg-black flex justify-center">
+    <Container>
+    Hello
+    </Container>
     </div>
   );
 };
---
+++ src/components/shared/Container.jsx
@@ -0,0 +1,7 @@
+const Container = () => {
+  return (
+    <div className="w-[60%] h-screen bg-green-300">Container</div>
+  )
+}
+
+export default Container
